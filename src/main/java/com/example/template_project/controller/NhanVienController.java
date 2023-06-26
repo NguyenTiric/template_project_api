713fa9755f3643342d981e0eaed3fa1ac167fd37
TiricNguyen\CongTien <nguyentiric@gmail.com>
2023-06-26 15:12:40 +0700

    update Controller

--- src/main/java/com/example/template_project/controller/NhanVienController.java
+++ src/main/java/com/example/template_project/controller/NhanVienController.java
@@ -1,8 +1,12 @@
 package com.example.template_project.controller;
 
+import com.example.template_project.service.ChucVuService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class NhanVienController {
     //demo
+    @Autowired
+    ChucVuService chucVuService;
 }
